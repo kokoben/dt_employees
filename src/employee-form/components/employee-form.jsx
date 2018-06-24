73813c89aef573fafe98cdb306882adc1061b79b
Ben <benzuvsteel@gmail.com>
2018-06-23 21:34:14 -0700

    added employee add form scaffolding

--- src/employee-form/components/employee-form.jsx
+++ src/employee-form/components/employee-form.jsx
@@ -3,20 +3,51 @@ import { Form, Input } from 'antd';
 
 const FormItem = Form.Item;
 
+const formItemLayout = {
+  labelCol: {
+    xs: { span: 24 },
+    sm: { span: 4 },
+  },
+  wrapperCol: {
+    xs: { span: 24 },
+    sm: { span: 20 },
+  },
+};
+
 const EmployeeForm = () => {
   return (
-    <div>
+    <div style={{width: '50%' }}>
       <h1>Employee Form</h1>
       <Form>
-        <FormItem label="First Name">
+        <FormItem
+          label="First Name"
+          {...formItemLayout}
+        >
+          <Input />
         </FormItem>
-        <FormItem label="Last Name">
+        <FormItem
+          label="Last Name"
+          {...formItemLayout}
+        >
+          <Input />
         </FormItem>
-        <FormItem label="Job Title">
+        <FormItem
+          label="Job Title"
+          {...formItemLayout}
+        >
+          <Input />
         </FormItem>
-        <FormItem label="Salary">
+        <FormItem
+          label="Salary"
+          {...formItemLayout}
+        >
+          <Input />
         </FormItem>
-        <FormItem label="Department">
+        <FormItem
+          label="Department"
+          {...formItemLayout}
+        >
+          <Input />
         </FormItem>
       </Form>
     </div>
